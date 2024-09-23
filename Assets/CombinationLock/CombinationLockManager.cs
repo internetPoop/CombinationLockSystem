b2ea9c0fe78d5fa20209a2a34d4d058beacc785b
KIKUSAN333 <166822198+KIKUSAN333@users.noreply.github.com>
2024-09-23 16:58:01 +0900

    Add SE and Delay
    効果音と解錠のアニメーションを最後まで再生するための遅延を追加しました.

--- Assets/CombinationLock/CombinationLockManager.cs
+++ Assets/CombinationLock/CombinationLockManager.cs
@@ -13,21 +13,30 @@ public class CombinationLockManager : MonoBehaviour
     [SerializeField] Button Button_1;
     [SerializeField] Button Button_2;
 
-    public void OnClickButton_0()
+    private AudioSource audioSource;
+    public AudioClip UnlockSE;  //解錠SE
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();  //オーディオソースを取得する
+    }
+
+
+    public void OnClickButton_0()   //ボタン0が押されたとき
     {
         Debug.Log("ClickButton0");
         ChangeNumber_0();
         CheckPassword();
     }
 
-    public void OnClickButton_1()
+    public void OnClickButton_1()   //ボタン1が押されたとき
     {
         Debug.Log("ClickButton1");
         ChangeNumber_1();
         CheckPassword();
     }
 
-    public void OnClickButton_2()
+    public void OnClickButton_2()   //ボタン2が押されたとき
     {
         Debug.Log("ClickButton2");
         ChangeNumber_2();
@@ -41,21 +50,21 @@ public class CombinationLockManager : MonoBehaviour
     void ChangeNumber_0()
     {
         NumberOfClickButton_0 = (NumberOfClickButton_0 + 1) % 10;   //１桁のみ表示させる
-        KeyText_0.text = NumberOfClickButton_0.ToString();
+        KeyText_0.text = NumberOfClickButton_0.ToString();  //テキスト変更
     }
 
     int NumberOfClickButton_1 = 0;
     void ChangeNumber_1()
     {
         NumberOfClickButton_1 = (NumberOfClickButton_1 + 1) % 10;   //１桁のみ表示させる
-        KeyText_1.text = NumberOfClickButton_1.ToString();
+        KeyText_1.text = NumberOfClickButton_1.ToString();  //テキスト変更
     }
 
     int NumberOfClickButton_2 = 0;
     void ChangeNumber_2()
     {
         NumberOfClickButton_2 = (NumberOfClickButton_2 + 1) % 10;   //１桁のみ表示させる
-        KeyText_2.text = NumberOfClickButton_2.ToString();
+        KeyText_2.text = NumberOfClickButton_2.ToString();  //テキスト変更
     }
 
 
@@ -75,7 +84,16 @@ public class CombinationLockManager : MonoBehaviour
 
             CombinationLockAnim.SetTrigger("Unlock_Trigger");   //Animator ControllerのUnlockTriggerをセットする
 
+            audioSource.PlayOneShot(UnlockSE);  //一度だけ効果音を鳴らす
+
+            
+            Invoke("AfterCorrectPassword", 2.0f);   //アニメーションを再生するために遅延
         }
+    }
+
+    void AfterCorrectPassword() //解錠が終わったあと
+    {
+        Debug.Log("AfterCorrectPassword");
     }
 
 }
